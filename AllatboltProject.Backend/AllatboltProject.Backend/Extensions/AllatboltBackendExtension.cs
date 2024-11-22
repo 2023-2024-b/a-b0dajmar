332315ff3b7435001e0612d6861506bc94d55d5e
Dajka Marcell <dajka.marcell@gmail.com>
2024-11-22 10:26:02 +0100

    cors

---
+++ AllatboltProject.Backend/AllatboltProject.Backend/Extensions/AllatboltBackendExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace AllatboltProject.Backend.Extensions
+{
+    public static class AllatboltBackendExtension
+    {
+        public static void AddBackend(this IServiceCollection services) { }
+        private static void ConfigureCors(IServiceCollection services)
+        {
+
+        }
+    }
+}
